272e72680c9f6f1757d9bd6bc2716eff8ea8bbeb
Daiho <qgik4271@yahoo.co.jp>
2025-04-26 20:21:58 +0900

    all変更

--- dice.py
+++ dice.py
@@ -1,20 +1,27 @@
 import random
 
-for i in range(10):
-    x = random.randint(1,6)
-    print(str(i+1)+"回目："+str(x))
+total = 0  # 合計値を初期化
 
-# 期待される出力結果例
+for i in range(10):
+    x = random.randint(1, 6)
+    total += x
+    print(str(i + 1) + "回目：" + str(x))
+
+average = total / 10  # 平均値を計算
+
+print("平均値：" + str(average))
+
+#期待される出力結果の例
 """
-1回目：4
-2回目：1
-3回目：1
-4回目：5
-5回目：1
-6回目：1
-7回目：5
-8回目：6
-9回目：4
-10回目：6
-平均値：3.4
+1回目：6
+2回目：3
+3回目：2
+4回目：2
+5回目：6
+6回目：5
+7回目：6
+8回目：1
+9回目：6
+10回目：2
+平均値：3.9
 """
